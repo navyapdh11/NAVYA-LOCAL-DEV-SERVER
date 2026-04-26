076022a95e65f6fbc6837ae6031ce334a962e918
Your Name <you@example.com>
2026-04-26 22:02:14 +0800

    feat: conduct multi-site benchmark round 15 and upgrade persistent memory with 67 total global profiles (Consulting cluster added)

--- benchmark_sites.py
+++ benchmark_sites.py
@@ -6,11 +6,11 @@ from typing import List, Dict
 
 BASE_URL = "http://localhost:8081"
 TARGET_SITES = [
-    "https://www.airbnb.com",
-    "https://www.booking.com",
-    "https://www.expedia.com",
-    "https://www.marriott.com",
-    "https://www.hilton.com"
+    "https://www.accenture.com",
+    "https://www2.deloitte.com",
+    "https://www.mckinsey.com",
+    "https://www.bcg.com",
+    "https://www.pwc.com"
 ]
 
 async def analyze_site(client: httpx.AsyncClient, url: str) -> Dict:
